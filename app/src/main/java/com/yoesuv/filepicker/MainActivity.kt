60eb252a4e86fe783a72522cd355826f9d9af871
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-06-08 17:26:46 +0700

    :pencil: setup view binding

--- app/src/main/java/com/yoesuv/filepicker/MainActivity.kt
+++ app/src/main/java/com/yoesuv/filepicker/MainActivity.kt
@@ -2,10 +2,19 @@ package com.yoesuv.filepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.filepicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonChooser.setOnClickListener {
+
+        }
     }
 }
