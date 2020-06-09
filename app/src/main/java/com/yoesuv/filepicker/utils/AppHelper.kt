4f09822439c3368b1c4cc3ab7f3ebe6d12a2d675
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-06-09 14:26:53 +0700

    :pencil: helper function

--- app/src/main/java/com/yoesuv/filepicker/utils/AppHelper.kt
+++ app/src/main/java/com/yoesuv/filepicker/utils/AppHelper.kt
@@ -1,2 +1,14 @@
 package com.yoesuv.filepicker.utils
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.yoesuv.filepicker.BuildConfig
+
+fun logDebug(message: String) {
+    if (BuildConfig.DEBUG) Log.d("result_debug", message)
+}
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
