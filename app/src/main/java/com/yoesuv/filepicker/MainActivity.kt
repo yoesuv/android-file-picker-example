c791c55464037a4332471fdba7e306e6c64b5b63
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-08-24 11:04:48 +0700

    :construction: start intent get content

--- app/src/main/java/com/yoesuv/filepicker/MainActivity.kt
+++ app/src/main/java/com/yoesuv/filepicker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.yoesuv.filepicker
 
 import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.filepicker.data.RC_PICK_FILE
 import com.yoesuv.filepicker.data.RC_READ_EXTERNAL_STORAGE
 import com.yoesuv.filepicker.databinding.ActivityMainBinding
 import com.yoesuv.filepicker.utils.hasPermission
@@ -31,15 +33,23 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private fun checkPermissionReadStorage() {
         if (hasPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
+            openFilePicker()
         } else {
             val rationale = getString(R.string.rationale_read_storage)
             EasyPermissions.requestPermissions(this, rationale, RC_READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
         }
     }
 
+    private fun openFilePicker() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+        startActivityForResult(intent, RC_PICK_FILE)
+    }
+
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
         logDebug("MainActivity # onPermissionsGranted -> request code : $requestCode/permissions $perms")
+        openFilePicker()
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
--- app/src/main/java/com/yoesuv/filepicker/data/Constants.kt
+++ app/src/main/java/com/yoesuv/filepicker/data/Constants.kt
@@ -2,3 +2,6 @@ package com.yoesuv.filepicker.data
 
 // req code permission
 const val RC_READ_EXTERNAL_STORAGE = 23
+
+// req code file
+const val RC_PICK_FILE = 31
