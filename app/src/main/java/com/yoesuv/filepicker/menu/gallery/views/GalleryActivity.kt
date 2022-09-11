c1ee960a78a410bbb1ae6b0a40c7cfc1696fa94f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-09-11 20:17:04 +0700

    :pencil: gallery permission

--- app/src/main/java/com/yoesuv/filepicker/menu/gallery/views/GalleryActivity.kt
+++ app/src/main/java/com/yoesuv/filepicker/menu/gallery/views/GalleryActivity.kt
@@ -4,19 +4,31 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.yoesuv.filepicker.R
 import com.yoesuv.filepicker.databinding.ActivityGalleryBinding
+import com.yoesuv.filepicker.menu.gallery.viewmodels.GalleryViewModel
+import com.yoesuv.filepicker.utils.showToast
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class GalleryActivity: AppCompatActivity() {
+class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivityGalleryBinding
+    private lateinit var viewModel: GalleryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_gallery)
         binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this)[GalleryViewModel::class.java]
+        binding.gallery = viewModel
 
         setupToolbar()
+
+        binding.buttonOpenGallery.setOnClickListener {
+            viewModel.clickOpenGallery(this)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -26,9 +38,30 @@ class GalleryActivity: AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private  fun setupToolbar() {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    private fun setupToolbar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.button_gallery)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        viewModel.openGallery()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            showToast(R.string.toast_permission_read_storage_denied)
+        }
     }
 
 }
