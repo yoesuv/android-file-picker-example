ee751dc1072b1d7d81312105baae0f7d6c23ccdd
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-09-18 12:06:29 +0700

    :pencil: create file

--- app/src/main/java/com/yoesuv/filepicker/menu/download/viewmodels/DownloadViewModel.kt
+++ app/src/main/java/com/yoesuv/filepicker/menu/download/viewmodels/DownloadViewModel.kt
@@ -8,8 +8,10 @@ import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.webkit.URLUtil
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.yoesuv.filepicker.data.DOWNLOAD_LINK
 import com.yoesuv.filepicker.utils.logDebug
 import java.io.File
@@ -41,34 +43,29 @@ class DownloadViewModel: ViewModel() {
         }
     }
 
-    // https://stackoverflow.com/a/65699427/3559183
+    /*
+     https://stackoverflow.com/a/65699427/3559183
+     https://stackoverflow.com/a/62879112/3559183
+    */
     @RequiresApi(Build.VERSION_CODES.Q)
     fun downloadFileSdk29(context: Context) {
-        val fileName = URLUtil.guessFileName(DOWNLOAD_LINK, null, null)
+        try {
+            val fileName = URLUtil.guessFileName(DOWNLOAD_LINK, null, null)
 
-        val fileCollection = MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-        //val uri = context.contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
-        val uri = context.contentResolver.insert(fileCollection, contentValues)
-        logDebug("DownloadViewModel # content resolver uri path : ${uri?.path}")
-        if (uri != null) {
-            context.contentResolver.openOutputStream(uri).use { outputStream ->
-                if (outputStream != null) {
-                    
-                }
+            val fileCollection = MediaStore.Files.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+            val uri = context.contentResolver.insert(fileCollection, contentValues)
+            logDebug("DownloadViewModel # content resolver uri path : ${uri?.path}")
+            if (uri != null) {
+                val outputStream = context.contentResolver.openOutputStream(uri, "rwt")
+                outputStream?.write("Save Pdf".toByteArray())
+                outputStream?.close()
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(context, "Error Download", Toast.LENGTH_SHORT).show()
         }
-
-//        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-//        val request = DownloadManager.Request(Uri.parse(DOWNLOAD_LINK))
-//        request.setAllowedOverMetered(true)
-//        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE)
-//        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
-//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-//        request.setTitle(fileName)
-//        request.setDestinationUri(uri)
-//        downloadManager.enqueue(request)
     }
 
 }
