4b3838ebeeb1d76f30d2a4e43c694fca5c1b9566
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-09-29 07:10:48 +0700

    :pencil: toast error

--- app/src/main/java/com/yoesuv/filepicker/menu/camera/viewmodels/CameraViewModel.kt
+++ app/src/main/java/com/yoesuv/filepicker/menu/camera/viewmodels/CameraViewModel.kt
@@ -7,10 +7,12 @@ import androidx.core.content.FileProvider
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yoesuv.filepicker.R
 import com.yoesuv.filepicker.data.COMPRESSOR_HEIGHT
 import com.yoesuv.filepicker.data.COMPRESSOR_QUALITY
 import com.yoesuv.filepicker.data.COMPRESSOR_WIDTH
 import com.yoesuv.filepicker.utils.logDebug
+import com.yoesuv.filepicker.utils.showToastError
 import id.zelory.compressor.Compressor
 import id.zelory.compressor.constraint.default
 import kotlinx.coroutines.launch
@@ -35,14 +37,16 @@ class CameraViewModel: ViewModel() {
 
     fun setPhotoUri(context: Context) {
         viewModelScope.launch {
-            photoFile?.let { photoFile ->
-                logDebug("CameraViewModel # file original path : ${photoFile.absolutePath}")
-                imagePath.postValue(photoFile.absolutePath)
-                val fileCompressed = Compressor.compress(context, photoFile) {
+            if (photoFile != null) {
+                logDebug("CameraViewModel # file original path : ${photoFile!!.absolutePath}")
+                imagePath.postValue(photoFile!!.absolutePath)
+                val fileCompressed = Compressor.compress(context, photoFile!!) {
                     default(width = COMPRESSOR_WIDTH, height = COMPRESSOR_HEIGHT, quality = COMPRESSOR_QUALITY)
                 }
                 logDebug("CameraViewModel # file compressed path : ${fileCompressed.absolutePath}")
                 imageFile.postValue(fileCompressed)
+            } else {
+                context.showToastError(R.string.toast_failed_get_image_camera)
             }
         }
     }
