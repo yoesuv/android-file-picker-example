31f71cb03ec1c6f031bcc0ba61d0f186e2f4b8c5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-02-02 16:44:12 +0700

    :construction: try allow permissions

--- app/src/androidTest/java/com/yoesuv/filepicker/PickerGalleryTest.kt
+++ app/src/androidTest/java/com/yoesuv/filepicker/PickerGalleryTest.kt
@@ -2,25 +2,24 @@ package com.yoesuv.filepicker
 
 import android.content.Context
 import android.os.SystemClock
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import com.yoesuv.filepicker.menu.main.views.MainActivity
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.FixMethodOrder
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.junit.runners.MethodSorters
 
 /**
@@ -38,6 +37,10 @@ class PickerGalleryTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+//    @get:Rule
+//    var galleryRule = GrantPermissionRule.grant(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+//    @get:Rule
+//    var galleryRule33 = GrantPermissionRule.grant(android.Manifest.permission.READ_MEDIA_IMAGES)
 
     @Before
     fun register() {
@@ -46,10 +49,22 @@ class PickerGalleryTest {
 
     @Test
     fun pickFromGallery() {
+        val instrumentation = InstrumentationRegistry.getInstrumentation()
+
         onView(ViewMatchers.withText(context.getString(R.string.button_gallery)))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         SystemClock.sleep(delay)
         onView(withId(R.id.btnMenuGallery)).perform(click())
         SystemClock.sleep(delay)
+
+        onView(withId(R.id.buttonOpenGallery)).perform(click())
+        SystemClock.sleep(delay)
+        SystemClock.sleep(delay)
+        val allowPermission = UiDevice.getInstance(instrumentation).findObject(
+            UiSelector().clickable(true).checkable(false).index(1)
+        )
+        if (allowPermission.exists()) {
+            allowPermission.click()
+        }
     }
 }
