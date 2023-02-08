0b7717ec9cfbd22294b8bfe580a60eaa51f982ba
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-02-08 13:54:27 +0700

    :umbrella: update test location

--- app/src/androidTest/java/com/yoesuv/filepicker/PickerLocationTest.kt
+++ app/src/androidTest/java/com/yoesuv/filepicker/PickerLocationTest.kt
@@ -2,11 +2,11 @@ package com.yoesuv.filepicker
 
 import android.content.Context
 import android.os.SystemClock
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -16,6 +16,7 @@ import androidx.test.uiautomator.UiSelector
 import com.yoesuv.filepicker.TestData.delay
 import com.yoesuv.filepicker.menu.main.views.MainActivity
 import com.yoesuv.filepicker.utils.IdlingResource
+import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.FixMethodOrder
@@ -50,19 +51,21 @@ class PickerLocationTest {
     fun pickLocationTest() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
 
-        Espresso.onView(ViewMatchers.withText(context.getString(R.string.button_location)))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText(context.getString(R.string.button_location)))
+            .check(matches(isDisplayed()))
         SystemClock.sleep(delay)
-        Espresso.onView(ViewMatchers.withId(R.id.btnMenuLocation)).perform(ViewActions.click())
+        onView(withId(R.id.btnMenuLocation)).perform(click())
         SystemClock.sleep(delay)
 
-        Espresso.onView(ViewMatchers.withId(R.id.buttonUserLocation)).perform(ViewActions.click())
+        onView(withId(R.id.buttonUserLocation)).perform(click())
         SystemClock.sleep(delay)
         val allowPermission = UiDevice.getInstance(instrumentation).findObject(
             UiSelector().text("While using the app")
         )
         if (allowPermission.exists()) {
             allowPermission.click()
+            SystemClock.sleep(delay)
+            onView(withId(R.id.tvUserLocation)).check(matches(not(withText(""))))
         }
 
         SystemClock.sleep(delay)
