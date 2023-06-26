59e8091dc7036c3c031f8f37d9e18c6b0d73a5c9
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-26 13:53:23 +0700

    :hammer: refactor permission gallery

--- app/src/main/java/com/yoesuv/filepicker/menu/gallery/views/GalleryActivity.kt
+++ app/src/main/java/com/yoesuv/filepicker/menu/gallery/views/GalleryActivity.kt
@@ -11,29 +11,35 @@ import com.bumptech.glide.Glide
 import com.yoesuv.filepicker.R
 import com.yoesuv.filepicker.data.PERM_READ_EXTERNAL_STORAGE
 import com.yoesuv.filepicker.data.PERM_READ_MEDIA_IMAGES
-import com.yoesuv.filepicker.data.RC_READ_EXTERNAL_STORAGE
 import com.yoesuv.filepicker.databinding.ActivityGalleryBinding
 import com.yoesuv.filepicker.menu.gallery.viewmodels.GalleryViewModel
 import com.yoesuv.filepicker.utils.hasPermission
 import com.yoesuv.filepicker.utils.isTiramisu
 import com.yoesuv.filepicker.utils.logDebug
 import com.yoesuv.filepicker.utils.showToastError
-import pub.devrel.easypermissions.AppSettingsDialog
-import pub.devrel.easypermissions.EasyPermissions
 
-class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+class GalleryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGalleryBinding
     private val viewModel: GalleryViewModel by viewModels()
 
-    private val startForResultGallery =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val uriData = result.data?.data
-                logDebug("GalleryActivity # uri : $uriData")
-                viewModel.setImageFile(this, uriData)
+    private val permissionGallery =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                openGallery()
+            } else {
+                val msg1 = R.string.rationale_media_images
+                val msg2 = R.string.rationale_read_storage_gallery
+                showToastError(if (isTiramisu()) msg1 else msg2)
             }
         }
+    private val resultGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val uriData = result.data?.data
+            logDebug("GalleryActivity # uri : $uriData")
+            viewModel.setImageFile(this, uriData)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,11 +59,6 @@ class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
     private fun setupToolbar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.button_gallery)
@@ -69,14 +70,7 @@ class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks
             if (hasPermission(this, thePermission)) {
                 openGallery()
             } else {
-                val msg1 = getString(R.string.rationale_media_images)
-                val msg2 = getString(R.string.rationale_read_storage_gallery)
-                EasyPermissions.requestPermissions(
-                    this,
-                    if (isTiramisu()) msg1 else msg2,
-                    RC_READ_EXTERNAL_STORAGE,
-                    thePermission
-                )
+                permissionGallery.launch(thePermission)
             }
         }
     }
@@ -84,7 +78,7 @@ class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        startForResultGallery.launch(intent)
+        resultGallery.launch(intent)
     }
 
     private fun observeData() {
@@ -93,21 +87,6 @@ class GalleryActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks
                 .centerCrop()
                 .dontAnimate()
                 .into(binding.ivGallery)
-        }
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-        openGallery()
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            AppSettingsDialog.Builder(this).build().show()
-        } else {
-            val msg1 = R.string.toast_permission_images_denied
-            val msg2 = R.string.toast_permission_read_storage_denied
-            val msgDenied = if (isTiramisu()) msg1 else msg2
-            showToastError(msgDenied)
         }
     }
 
