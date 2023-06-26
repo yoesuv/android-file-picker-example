7610802f0304a9f12fecdbefd4a98206c7bca822
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-27 05:30:02 +0700

    :newspaper: add record audio viewmodel

---
+++ app/src/main/java/com/yoesuv/filepicker/menu/record/viewmodels/RecordAudioViewModel.kt
@@ -0,0 +1,15 @@
+package com.yoesuv.filepicker.menu.record.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.yoesuv.filepicker.data.RecordingState
+
+class RecordAudioViewModel: ViewModel() {
+
+    var recordState = MutableLiveData<RecordingState>()
+
+    init {
+        recordState.value = RecordingState.START
+    }
+
+}
--- app/src/main/java/com/yoesuv/filepicker/menu/record/views/RecordAudioActivity.kt
+++ app/src/main/java/com/yoesuv/filepicker/menu/record/views/RecordAudioActivity.kt
@@ -2,21 +2,26 @@ package com.yoesuv.filepicker.menu.record.views
 
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.yoesuv.filepicker.R
 import com.yoesuv.filepicker.databinding.ActivityRecordAudioBinding
+import com.yoesuv.filepicker.menu.record.viewmodels.RecordAudioViewModel
 
 class RecordAudioActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityRecordAudioBinding
+    private val viewModel: RecordAudioViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_record_audio)
         binding.lifecycleOwner = this
+        binding.record = viewModel
 
         setupToolbar()
+        observeData()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -29,6 +34,13 @@ class RecordAudioActivity: AppCompatActivity() {
     private fun setupToolbar() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setTitle(R.string.button_record_audio)
+    }
+
+    private fun observeData() {
+        viewModel.recordState.observe(this) {
+            val str = getString(R.string.is_recording, it.name)
+            binding.tvRecordingState.text = str
+        }
     }
 
 }
