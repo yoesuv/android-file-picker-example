4a3633c9ce51eb6ad2971a271359af753b3aae6b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-09-30 05:39:15 +0700

    :arrow_up: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.6.1" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
 }
